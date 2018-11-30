379d430802483743236a3870c102294a8280b420
Iago C <iagocrpi@gmail.com>
2018-11-30 23:27:46 +0100

    Primeros pasos. Añadido App y StorePicker

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import StorePicker from "./StorePicker";
+
+class App extends React.Component {
+  render() {
+    return <StorePicker />;
+  }
+}
+console.log("¿Funciona?");
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.info("Wired UP 💎");
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.querySelector("#app-container"));
